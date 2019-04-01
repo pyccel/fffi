eba3245d8cd278d7917fef0dbe101d192f345c3b
ratnania <ratnaniahmed@gmail.com>
2019-04-01 16:57:05 +0200

    [fparser] start custom classes for textx

--- fffi/parser/parser.py
+++ fffi/parser/parser.py
@@ -2,7 +2,34 @@
 
 import os
 from os.path import join, dirname
+
 from textx.metamodel import metamodel_from_str
+
+from pyccel.ast import Variable
+from pyccel.ast.datatypes import dtype_and_precsision_registry as dtype_registry
+
+#==============================================================================
+class Fortran(object):
+    """Class for Fortran syntax."""
+    def __init__(self, *args, **kwargs):
+        self.statements = kwargs.pop('statements', [])
+
+class Declaration(object):
+    """Class representing a Fortran declaration."""
+    def __init__(self, **kwargs):
+        """
+        """
+        self.dtype     = kwargs.pop('dtype')
+        self.attributs = kwargs.pop('attributs', []) # this is optional
+        self.entities  = kwargs.pop('entities')
+
+class DeclarationEntityObject(object):
+    """Class representing an entity object ."""
+    def __init__(self, **kwargs):
+        """
+        """
+        self.name = kwargs.pop('name')
+#==============================================================================
 
 #==============================================================================
 def get_by_name(ast, name):
@@ -31,8 +58,12 @@ def parse(inputs, debug=False):
     # Get meta-model from language description
     grammar = join(this_folder, 'grammar.tx')
 
+    classes = [Fortran,
+               Declaration,
+               DeclarationEntityObject]
+
     from textx.metamodel import metamodel_from_file
-    meta = metamodel_from_file(grammar, debug=debug) # TODO add classes
+    meta = metamodel_from_file(grammar, debug=debug, classes=classes)
 
     # Instantiate model
     if os.path.isfile(inputs):
@@ -41,21 +72,52 @@ def parse(inputs, debug=False):
     else:
         ast = meta.model_from_str(inputs)
 
-    for token in ast.statements:
-        print(token)
+    namespace = {}
+    stmts = []
+    for stmt in ast.statements:
+        if isinstance(stmt, Declaration):
+            dtype = stmt.dtype.kind
+            if dtype.upper() == 'DOUBLE PRECISION':
+                dtype = 'DOUBLE'
+            dtype, precision = dtype_registry[dtype.lower()]
+            attributs = [i.lower() for i in stmt.attributs]
+            entities = stmt.entities
+            names = [i.name for i in entities]
+#            print(dtype, attributs, names)
 
-#    stmts = []
-#    for stmt in model.statements:
-#        if isinstance(stmt, OpenmpStmt):
-#            e = stmt.stmt.expr
-#            stmts.append(e)
-#
-#    if len(stmts) == 1:
-#        return stmts[0]
-#    else:
-#        return stmts
+            rank = 0
+
+            is_allocatable = ('allocatable' in attributs)
+            is_pointer     = ('pointer' in attributs)
+            is_target      = ('target' in attributs)
+
+            for name in names:
+                var = Variable( dtype,
+                                name,
+                                rank           = rank,
+                                allocatable    = is_allocatable,
+#                                is_stack_array = ,
+                                is_pointer     = is_pointer,
+                                is_target      = is_target,
+#                                is_polymorphic=,
+#                                is_optional=,
+#                                shape=None,
+#                                cls_base=None,
+#                                cls_parameters=None,
+#                                order=,
+                                precision=precision,
+                              )
+
+                namespace[name] = var
+
+        else:
+            raise NotImplementedError('TODO {}'.format(type(stmt)))
+
+
+    print(namespace)
 
 
 ####################################
 if __name__ == '__main__':
     ast = parse('INTEGER :: x')
+    ast = parse('DOUBLE PRECISION :: y')
