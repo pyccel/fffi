7ee51bd7212a7a0bb7c6b346834e040203ac9f8b
ratnania <ratnaniahmed@gmail.com>
2019-04-01 19:11:40 +0200

    [fparser] start array spec

--- fffi/parser/parser.py
+++ fffi/parser/parser.py
@@ -17,18 +17,58 @@ class Fortran(object):
 class Declaration(object):
     """Class representing a Fortran declaration."""
     def __init__(self, **kwargs):
-        """
-        """
         self.dtype     = kwargs.pop('dtype')
         self.attributs = kwargs.pop('attributs', []) # this is optional
         self.entities  = kwargs.pop('entities')
 
+class DeclarationAttribut(object):
+    """Class representing a Fortran declaration attribut."""
+    def __init__(self, **kwargs):
+        self.key   = kwargs.pop('key')
+        self.value = kwargs.pop('value', None) # this is optional
+
 class DeclarationEntityObject(object):
     """Class representing an entity object ."""
     def __init__(self, **kwargs):
-        """
-        """
         self.name = kwargs.pop('name')
+
+class DeclarationEntityFunction(object):
+    """Class representing an entity function."""
+    def __init__(self, **kwargs):
+        self.name = kwargs.pop('name')
+        self.charlen = kwargs.pop('charlen', None)
+        raise NotImplementedError('')
+
+class ArraySpec(object):
+    """Class representing array spec."""
+    def __init__(self, **kwargs):
+        self.value = kwargs.pop('value')
+
+    @property
+    def expr(self):
+        d_infos = {}
+
+        shapes = []
+        if isinstance(self.value, (list, tuple)):
+            for i in self.value:
+                if isinstance(i, ArrayExplicitShapeSpec):
+                    lower = i.lower_bound
+                    upper = i.upper_bound
+                    shapes.append([lower, upper])
+
+        else:
+            raise NotImplementedError('')
+
+        d_infos['shape'] = shapes
+        return d_infos
+
+
+class ArrayExplicitShapeSpec(object):
+    """Class representing explicit array shape."""
+    def __init__(self, **kwargs):
+        self.upper_bound = kwargs.pop('upper_bound')
+        self.lower_bound = kwargs.pop('lower_bound', None)
+
 #==============================================================================
 
 #==============================================================================
@@ -60,7 +100,11 @@ def parse(inputs, debug=False):
 
     classes = [Fortran,
                Declaration,
-               DeclarationEntityObject]
+               DeclarationAttribut,
+               DeclarationEntityObject,
+               DeclarationEntityFunction,
+               ArraySpec,
+               ArrayExplicitShapeSpec]
 
     from textx.metamodel import metamodel_from_file
     meta = metamodel_from_file(grammar, debug=debug, classes=classes)
@@ -76,16 +120,34 @@ def parse(inputs, debug=False):
     stmts = []
     for stmt in ast.statements:
         if isinstance(stmt, Declaration):
+            # ...
             dtype = stmt.dtype.kind
             if dtype.upper() == 'DOUBLE PRECISION':
                 dtype = 'DOUBLE'
             dtype, precision = dtype_registry[dtype.lower()]
-            attributs = [i.lower() for i in stmt.attributs]
+            # ...
+
+            # ...
             entities = stmt.entities
             names = [i.name for i in entities]
-#            print(dtype, attributs, names)
+            # ...
 
+            # ...
             rank = 0
+            attributs = []
+            for i in stmt.attributs:
+                key   = i.key.lower()
+                value = i.value
+                d_infos = value.expr
+
+                if key == 'dimension':
+                    shape = d_infos['shape']
+                    rank = len(shape)
+
+                attributs.append(key)
+            # ...
+
+#            print(dtype, attributs, names)
 
             is_allocatable = ('allocatable' in attributs)
             is_pointer     = ('pointer' in attributs)
@@ -101,7 +163,7 @@ def parse(inputs, debug=False):
                                 is_target      = is_target,
 #                                is_polymorphic=,
 #                                is_optional=,
-#                                shape=None,
+                                shape=shape,
 #                                cls_base=None,
 #                                cls_parameters=None,
 #                                order=,
@@ -114,10 +176,15 @@ def parse(inputs, debug=False):
             raise NotImplementedError('TODO {}'.format(type(stmt)))
 
 
-    print(namespace)
+#    print(namespace)
+    for k,v in namespace.items():
+        v.inspect()
 
 
 ####################################
 if __name__ == '__main__':
-    ast = parse('INTEGER :: x')
-    ast = parse('DOUBLE PRECISION :: y')
+#    ast = parse('INTEGER :: x')
+#    ast = parse('INTEGER  x')
+#    ast = parse('INTEGER, PARAMETER ::  x')
+#    ast = parse('DOUBLE PRECISION :: y')
+    ast = parse('INTEGER, DIMENSION(10) :: x')
