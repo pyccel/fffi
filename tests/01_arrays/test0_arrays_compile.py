f70c315159651aa16fc6e68e661c1681d26ca7db
Christopher Albert <albert@alumni.tugraz.at>
2019-04-03 10:45:32 +0200

    Path specifications, unit tests work

--- tests/01_arrays/test0_arrays_compile.py
+++ tests/01_arrays/test0_arrays_compile.py
@@ -5,13 +5,12 @@ Created: 2019-04-01
 Compiles CFF for test_arrays
 """
 
-import numpy as np
-import inspect
 import os
-
+from shutil import copy, rmtree
+from tempfile import mkdtemp
 from unittest import TestCase
-
 from fffi import fortran_module
+
 
 class TestArraysCompile(TestCase):
 
@@ -19,31 +18,39 @@ class TestArraysCompile(TestCase):
     def setUpClass(cls):
         cls.origcwd = os.getcwd()
         cls.cwd = os.path.dirname(__file__)
-        os.chdir(cls.cwd)
+        cls.tmpdir = mkdtemp()
+
+        copy(os.path.join(cls.cwd, 'Makefile'), cls.tmpdir)
+        copy(os.path.join(cls.cwd, 'mod_arrays.f90'), cls.tmpdir)
+        copy(os.path.join(cls.cwd, 'test_arrays.f90'), cls.tmpdir)
+        os.mkdir(os.path.join(cls.tmpdir, 'static'))
+        os.mkdir(os.path.join(cls.tmpdir, 'shared'))
+
+        os.chdir(cls.tmpdir)
 
         try:
-            os.system('make clean && make')
-        except:
+            os.system('make')
+        except BaseException:
             os.chdir(cls.origcwd)
             raise
-
 
     @classmethod
     def tearDownClass(cls):
         os.chdir(cls.origcwd)
-
+        rmtree(cls.tmpdir)
 
     def test_compile(self):
         # Initialize
-        mod_arrays = fortran_module('test_arrays', 'mod_arrays')
+        mod_arrays = fortran_module('test_arrays', 'mod_arrays',
+                                    path=self.tmpdir)
 
         # This will use fortran_module.fdef instead of cdef in the future.
-        # Ideally, the actual Fortran source file would be parsed as an option
-        # instead of code containing only the signatures.
+        # Ideally, the actual Fortran source file would be parsed as an
+        # option instead of code containing only the signatures.
 
         mod_arrays.cdef("""
           void {mod}_test_vector(array_1d *vec);
           void {mod}_test_array_2d(array_2d *arr);
         """)
 
-        mod_arrays.compile(verbose=False)
+        mod_arrays.compile(tmpdir=self.tmpdir)
